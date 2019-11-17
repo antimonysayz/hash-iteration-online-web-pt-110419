9f40a55f8448fb20c72a83509564f3687d190de6
Anthony Mai <anthony.mai530@gmail.com>
2019-11-17 03:41:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,13 +7,13 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     puts "Happy Birthday #{name}! You are now #{age} years old!"
-    end
   end
 end
 
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
+    if age < 12
     puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
   end
